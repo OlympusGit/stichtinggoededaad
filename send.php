c9bed6ee00392a7f9a7c18e8ab5f8c5aab05bdd3
stichtinggoededaad <koekjemel@hotmail.com>
2018-04-19 03:10:11 +0200

    lol
    Signed-off-by: stichtinggoededaad <koekjemel@hotmail.com>
    
    Signed-off-by: stichtinggoededaad <koekjemel@hotmail.com>

--- send.php
+++ send.php
@@ -3,24 +3,9 @@
 
 <p>Required fields are <b>bold</b></p>
 
-<form action="contactsend.php" method="post">
-<p><b>Your Name:</b> <input type="text" name="naam" /><br />
-<b>Subject:</b> <input type="text" name="onderwerp" /><br />
-<b>E-mail:</b> <input type="text" name="email" /><br />
-bedrijf: <input type="text" name="bedrijf"></p>
-straat: <input type="text" name="straat"></p>
-postcode: <input type="text" name="postcode"></p>
-gemeente: <input type="text" name="gemeente"></p>
-land: <input type="text" name="land"></p>
-telefoon: <input type="text" name="telefoon"></p>
 
 
-<p>Do you like this website?
-<input type="radio" name="likeit" value="Yes" checked="checked" /> Yes
-<input type="radio" name="likeit" value="No" /> No
-<input type="radio" name="likeit" value="Not sure" /> Not sure</p>
 
-<p>How did you find us?
 <select name="how">
 <option value=""> -- Please select -- </option>
 <option>Google</option>
